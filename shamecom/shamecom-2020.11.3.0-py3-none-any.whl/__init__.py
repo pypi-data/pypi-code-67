5d95bb883edae23106c4181ad78b6b402c26cde5
Tom Forbes <tom@tomforb.es>
2020-11-03 14:17:23 +0000

    {"name":"shamecom","version":"2020.11.3.0","file":"shamecom-2020.11.3.0-py3-none-any.whl","path":"shamecom/shamecom-2020.11.3.0-py3-none-any.whl"}

---
+++ shamecom/shamecom-2020.11.3.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shamecom
+https://github.com/apachecn/shamecom"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2020.11.3.0"
